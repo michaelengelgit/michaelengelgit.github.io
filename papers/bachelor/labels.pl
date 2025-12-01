1d1306a6ea5fb05450b3ce9537ef2c5c5be5f645
michael <michael@mail.com>
2025-12-01 18:51:59 +0100

    add bachelor thesis

---
+++ papers/bachelor/labels.pl
@@ -0,0 +1,49 @@
+# LaTeX2HTML 2025 (Released January 1, 2025)
+# Associate labels original text with physical files.
+
+
+$key = q/code:getlang/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/code:table/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ds:normal/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ds:tl/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/img:table/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/img:table_pic/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pic:comment/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pic:dokuwiki/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pic:wiki/;
+$external_labels{$key} = "$URL/" . q|bachelorarbeit_michael_engel.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2025 (Released January 1, 2025)
+# labels from external_latex_labels array.
+
+
+1;
+
